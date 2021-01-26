0fd3deb671278cf5dc2035d81a3350c04f6b616c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-26 16:10:54 -0500

    Add more logic to AddCartItem function

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => {
   
 const getTotalItems = () => null
 
-const handleAddToCart = () => null
+const handleAddToCart = (clickedItem: CartItemType) => null
 
 const handleRemoveFromCart = () => null
 
