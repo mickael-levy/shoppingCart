bf6e6f9abe1d0c1e7d24de457123e404bc73408f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-01-25 21:03:23 -0500

    Attempt to display data to the console

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,13 @@ export type CartItemType = {
   amount: number;
 }
 
-const getProducts = async (): Promise<CartItemType> =>
+const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products, getProducts')
+  console.log(data);
+  
   return <div className="App"> Start </div>;
 };
 
