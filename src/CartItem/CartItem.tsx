b4b4be4af2943e2dad5dd6e8df38c0df5e567eea
mickael-levy <mickael.levy977@gmail.com>
2021-02-13 20:11:21 -0500

    Include how to display price and calculate the total in our cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -5,6 +5,22 @@ import { CartItemType } from '../App';
 //styles
 import { Wrapper } from './CartItem.styles';
 
-const CartItem: React.FC = () => <div>Cart Item</div>;
+type Props = {
+    item: CartItemType
+    addToCart: (clickedItem: CartItemType) => void
+    removeFromCart: (id: number) => void
+}
+
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+        </div>
+    </Wrapper>
+)
 
 export default CartItem
