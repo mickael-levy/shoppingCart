e0d3108ad192aeb8c063ba1b7b1a96100adc0245
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-13 22:00:13 -0500

    Style items in cart, including button and img

--- src/CartItem/CartItem.styles.ts
+++ src/CartItem/CartItem.styles.ts
@@ -4,6 +4,21 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
     font-family: Arial, Helvetica, sans-serif;
-    border-bottom: 1px solid light blue;
+    border-bottom: 1px solid lightblue;
     padding-buttom: 20px;
+
+    div {
+        flex: 1;
+    }
+
+    .information, .button {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    img {
+        max-width: 80px;
+        object-fit: cover;
+        margin-left: 40px;
+    }
 `
