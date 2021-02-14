8c0fd67a4afed084986670fb915c62b7a9c8c051
mickael-levy <mickael.levy977@gmail.com>
2021-02-13 21:20:18 -0500

    Add buttons for cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,4 +1,4 @@
-import Buttom from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 
 //types
 import { CartItemType } from '../App';
@@ -19,8 +19,28 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
                 <p>Price: ${item.price}</p>
                 <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
             </div>
+            <div className="buttons">
+                <Button 
+                    size="small"
+                    disableElevation
+                    variant="contained"
+                    onClick={() => removeFromCart(item.id)}
+                >
+                    -
+                </Button>
+                <p>{item.amount}</p>
+                <Button 
+                    size="small"
+                    disableElevation
+                    variant="contained"
+                    onClick={() => addToCart(item)}
+                >
+                    +
+                </Button>
+            </div>
         </div>
+        <img src={item.image} alt={item.title} />
     </Wrapper>
 )
 
-export default CartItem
+export default CartItem 
