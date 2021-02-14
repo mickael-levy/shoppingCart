83dd446d7beb9133d435b5001c28ca10f122e8e2
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-13 19:03:08 -0500

    Include user messages for cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,1 +1,21 @@
-import 
+import CartItem from '../CartItem/CartItem'
+//styles
+import { Wrapper } from './Cart.styles'
+//types
+import { CartItemType } from '../App'
+
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void; 
+    removeFromCart: (id: number) => void;
+}
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
+
+    return (
+        <Wrapper>
+            <h2>Your Cart</h2>
+            {cartItems.length === 0 ? <p>Your Cart Is Empty</p> : null}
+        </Wrapper>
+    )
+}
