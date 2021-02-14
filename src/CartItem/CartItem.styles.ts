0bb49828ade3ab6e9a96d55e46006a50b897a3c8
mickael-levy <mickael.levy977@gmail.com>
2021-02-13 21:55:09 -0500

    Style items in cart

--- src/CartItem/CartItem.styles.ts
+++ src/CartItem/CartItem.styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-family: Arial, Helvetica, sans-serif;
+    border-bottom: 1px solid light blue;
+    padding-buttom: 20px;
+`
